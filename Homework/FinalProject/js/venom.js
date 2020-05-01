2c169e0c2fde5c7f63c45be64995b318a8a9d4bb
Beth Mosch <your_email_address@email.com>
2020-05-01 10:56:30 -0500

    Final Project

---
+++ Homework/FinalProject/js/venom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+
+	// var is_mobile = false;
+  //   if( $('.video-wrap video').css('display')=='none') {
+  //       is_mobile = true;
+  //   }
+  //   if (is_mobile == true) {
+	// 			$(".desktopGrid").remove();
+  //   }
+	// 	else if (is_mobile == false) {
+	// 		$(".mobileGrid").remove();
+	// 	}
+
+
+	$('#pagepiling').pagepiling(); //You need to initialize the the div with the pagepiling function();
+
+
+	// var div1 = $(".fundGrid div:first-child i").offset();
+	// var div2 = $(".fundGrid div:nth-child(2) i").offset();
+	// var div3 = $(".fundGrid div:nth-child(3) i").offset();
+	//
+	//
+	// $(window).scroll(function() {
+	// 	var wScroll = $(this).scrollTop();
+	//
+	// 	if(wScroll > div1.top - 200) {
+	// 			//do stuff
+	// 			$(".fundGrid div:first-child i").addClass('anim');
+	// 	}
+	// 	if(wScroll > div2.top - 200) {
+	// 			//do stuff
+	// 			$(".fundGrid div:nth-child(2) i").addClass('anim');
+	// 	}
+	// 	if(wScroll > div3.top - 200) {
+	// 			//do stuff
+	// 			$(".fundGrid div:nth-child(3) i").addClass('anim');
+	// 	}
+	// });
+
+
+});
